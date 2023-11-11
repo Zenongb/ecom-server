8b6513b8da191f2b86d4877653e02e0e1bb9e550
z <zenongb@hotmail.com>
2023-11-11 10:52:50 -0300

    product path creation when generation dummy data

--- src/productGenerator.js
+++ src/productGenerator.js
@@ -1,10 +1,27 @@
 // script para generar productos randomizados para testeo
+import fs from "fs/promises"
 import ProductManager from "./ProductManager.js"
 import { PRODUCTS_PATH } from "./lib.js"
 
+const AMT = 10
+
+// check si el path existe
+
+const pathArr = PRODUCTS_PATH.split("/")
+
+// agarrar directorios, ya que el pm handlea el archivo inexistente. Tiene una
+// inconsistencia con los paths relativos, ya que estos comienzan con "./", que
+// en el split genera un primer item de la forma ".", por eso desestimar el 
+// primer item. Esta funcionalidad deberia ser modificada
+const dirs = pathArr.slice(1,-1)
+
+// crear recursivamente los directorios
+await fs.mkdir(dirs.join("/"), {recursive: true})
+
+// una vez creados los directorios, proceder con la creacion de los productos
+
 const pm = new ProductManager(PRODUCTS_PATH)
 
-const AMT = 10
 
 for (let i = 0; i < AMT; i++) {
   await pm.addProduct(
