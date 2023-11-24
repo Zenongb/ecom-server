0da40a6e179bb378edb548b4d0dab51d71f46925
z <zenongb@hotmail.com>
2023-11-23 23:16:28 -0300

    implemented delete interface

--- static/js/realtimeProducts.client.js
+++ static/js/realtimeProducts.client.js
@@ -1,46 +1,61 @@
-import { updateTable } from "./lib.js"
+import { updateTable } from "./lib.js";
 
-const socket = io()
+// Websockets handling
+const socket = io();
 
 socket.on("connect", () => {
-  console.log("conectado")
-})
+  console.log("conectado");
+});
 
-socket.on("updateProducts", (products) => {
-  updateTable(products)
-})
+socket.on("updateProducts", products => {
+  updateTable(products);
+});
 
-const appProdForm = document.querySelector("form")
+const addProdForm = document.getElementById("addProduct");
 
-appProdForm.addEventListener("submit", (event) => {
-  event.preventDefault()
-  const prodSubmit = {}
-  for (let formChild of appProdForm.childNodes) {
-
+addProdForm.addEventListener("submit", event => {
+  event.preventDefault();
+  const prodSubmit = {};
+  for (let formChild of addProdForm.childNodes) {
     // console.log(formChild)
-    if (formChild.nodeName === "LABEL") { // encontramos una label
+    if (formChild.nodeName === "LABEL") {
+      // encontramos una label
       // console.log(formChild)
-      const key = formChild.lastChild.attributes.name.value
-      const value = formChild.lastChild.value
-      prodSubmit[key] = value
+      const key = formChild.lastChild.attributes.name.value;
+      const value = formChild.lastChild.value;
+      prodSubmit[key] = value;
       if (key === "status") {
-        console.log(key, value)
-        prodSubmit[key] = value === "on"
-        console.log(prodSubmit[key])
+        console.log(key, value);
+        prodSubmit[key] = value === "on";
+        console.log(prodSubmit[key]);
       }
     }
   }
-  console.log("sending form")
-  console.log(prodSubmit)
+  console.log("sending form");
+  console.log(prodSubmit);
   fetch("/api/products", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
-    body: JSON.stringify(prodSubmit)
-  }).then(res => {
-    console.log(res.status)
-  }).catch(err => {
-    console.log(err)
+    body: JSON.stringify(prodSubmit),
   })
-})
+    .then(res => {
+      console.log(res.json());
+    })
+    .catch(err => {
+      console.log(err);
+    });
+});
+
+document.getElementById("deleteProduct").addEventListener("submit", event => {
+  event.preventDefault();
+  const pid = document.getElementById("pid").value;
+  console.log(pid);
+  fetch(`/api/products/${pid}`, {
+    method: "DELETE",
+  })
+    .then(res => res.json())
+    .then(res => console.log(res))
+    .catch(err => console.error(err))
+});
