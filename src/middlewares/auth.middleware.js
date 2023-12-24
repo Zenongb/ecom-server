e7cab8c3bc2f39a6637222654d2d0d1d28f759a9
z <zenongb@hotmail.com>
2023-12-24 12:35:45 -0300

    implemented auth

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -4,12 +4,38 @@ import MongoStore from "connect-mongo"
 
 import { AUTH_DB_URL, SESSION_SECRET } from "../config.js"
 
-const store = MongoStore.create({
-  mongoUrl: AUTH_DB_URL
-})
+const store = connectMongo.create({
+  mongoUrl: AUTH_DB_URL,
+  ttl: 60 * 60 * 24, // 1d
+});
 
-export const sessionHandler = session({
+// "Middleware" de setup de sessiones y store
+export const sessions = session({
   store,
   secret: SESSION_SECRET,
-  ttl: 60 * 60 * 6
-})
+  resave: false,
+  saveUninitialized: false,
+});
+
+// middleware de autorización por privilegios
+export const auth = role => {
+  return (req,res,next) => {
+    const user = req.session.user
+    if (user?.role === role) {
+      // coinciden los privilegios
+      next()
+    } else if (user?.isLoggedIn){
+      // no coinciden y hay un usuario logueado
+      return res.status(403).json({
+        status: "error",
+        message: "user forbidden to enter this page"
+      })
+    } else {
+      // no coinciden y no hay user logueado
+      return res.status(401).json({
+        status: "unauthorized",
+        message: "log in first"
+      })
+    }
+  }
+}
